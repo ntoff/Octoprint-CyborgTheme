e914978c9fd143d1ea253ef92dfc264a38ccffc8
ntoff <norahtihtzoff@gmail.com>
2017-07-26 02:41:00 +1000

    Add a body class identifier and remove the issue template
    Body class identifier added and removed the issue template, it sounded a
    bit harsh (wasn't intentional)

---
+++ octoprint_cyborgtheme/static/js/octoprint-cyborgtheme.js
@@ -0,0 +1,13 @@
+$(function() {
+    function CyborgThemePluginViewModel(parameters) {
+        var self = this;
+
+        $("body").eq(0).addClass("OctoPrintTheme-Cyborg")
+
+        }	
+		OCTOPRINT_VIEWMODELS.push([
+        CyborgThemePluginViewModel,
+		[]
+		
+    ]);
+});

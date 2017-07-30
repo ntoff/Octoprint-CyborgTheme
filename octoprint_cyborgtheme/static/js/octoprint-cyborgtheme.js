9ff51f0f7f6bb2ebc644d6cb00d7b9816f744d89
ntoff <norahtihtzoff@gmail.com>
2017-07-30 16:01:45 +1000

    prelim test default icon colour white

--- octoprint_cyborgtheme/static/js/octoprint-cyborgtheme.js
+++ octoprint_cyborgtheme/static/js/octoprint-cyborgtheme.js
@@ -3,6 +3,7 @@ $(function() {
         var self = this;
 
         $("body").eq(0).addClass("OctoPrintTheme-Cyborg")
+        $("#settings_dialog").eq(0).addClass("Cyborgified_Settings")
 
         }	
 		OCTOPRINT_VIEWMODELS.push([
